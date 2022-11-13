73c31f4d349966d84c6c49cf808a2f1c8c29ca6c
zoent <113313951+zoent@users.noreply.github.com>
2022-11-13 12:52:21 -0800

    Update test.py

--- src/test.py
+++ src/test.py
@@ -5,5 +5,9 @@ from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 
 segments = [ 63, 6, 91, 79, 102, 109, 124, 7, 127, 103 ]
 
-@cocotb.test()
-return 1;
+async def test_7seg(dut):
+    dut._log.info("start")
+    clock = Clock(dut.clk, 10, units="us")
+    cocotb.fork(clock.start())
+
+    await ClockCycles(dut.clk, 10)
