1ac9a7ee4b93eac4014a14567da6198ad6189dcf
zoent <113313951+zoent@users.noreply.github.com>
2022-11-13 11:57:15 -0800

    Update tb.v

--- src/tb.v
+++ src/tb.v
@@ -8,9 +8,9 @@ that can be driven / tested by the cocotb test.py
 
 module tb (
     // testbench is controlled by test.py
-    input clk,
-    input rst,
-    output [6:0] segments
+    input clk
+    ,input [6:0] data
+    ,output [6:0] segments
    );
 
     // this part dumps the trace to a vcd file that can be viewed with GTKWave
@@ -21,7 +21,7 @@ module tb (
     end
 
     // wire up the inputs and outputs
-    wire [7:0] inputs = {6'b0, rst, clk};
+    wire [7:0] inputs = {data, clk};
     wire [7:0] outputs;
     assign segments = outputs[6:0];
 
