89e757ca1304cbefc903ed1c39ef0dab2345963c
zoent <113313951+zoent@users.noreply.github.com>
2022-11-13 16:25:03 -0800

    Update zoechip.v

--- src/zoechip.v
+++ src/zoechip.v
@@ -22,19 +22,11 @@
 
 module zoechip #( parameter MAX_COUNT = 1000 ) (
   input [7:0] io_in,
- output [8:1] io_out
+  output [7:0] io_out
 );
 
 wire A,B,C,D,F,G,M;
-
-assign io_out[1] = A;
-assign io_out[6] = B;
-assign io_out[2] = C;
-assign io_out[7] = D;
-assign io_out[4] = F;
-assign io_out[5] = G;
-assign io_out[3] = M;
-assign io_out[8] = 0;
+assign io_out = { 1'b0, D,B,G,F,M,C,A };
 
 wire Z = io_in[0];
 wire O = io_in[1];
